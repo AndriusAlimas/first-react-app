a0d14a8288bc8aea8079e495263667d4ee75439e
Andrius Alimas <XandrejoX@yahoo.com>
2023-11-09 14:57:37 +0000

    component reusable with props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
-import LogoImg from "./assets/logo192.png";
+import logoImg from "./assets/logo192.png";
+import componentsImage from "./assets/components.png";
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcept({ title, image, text }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>${title}</h3>
+      <p>{text}</p>
+    </li>
+  );
 }
 
 function Header() {
@@ -11,23 +22,37 @@ function Header() {
     reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
   return (
     <header className="App-header">
-      <header>
-        <img src={LogoImg} alt="Stylized atom" className="App-logo" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <img src={logoImg} alt="Stylized atom" className="App-logo" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
     </header>
   );
 }
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              image={componentsImage}
+              title="React"
+              text="This is my text"
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
